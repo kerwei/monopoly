212e1d8c8066c5ea7ad23ee381cc4c862a498e1f
kerwei <kerwei84@gmail.com>
2020-06-30 00:17:14 +0800

    Add the board class and some basic methods

--- board.py
+++ board.py
@@ -1,3 +1,31 @@
+import player
+
+
+class Board:
+    """
+    The main board game class
+    """
+    def __init__(self, lst_player: list, schema: dict):
+        # For now players are added based on list sequence. A method will be
+        # added to determine the turn of each player later
+        self.roster = {
+            i: player.Player() for i, name in lst_player
+        }
+        self.schema = schema
+
+    def assign_turns(self):
+        """
+        Assign the turn for each player
+        """
+        raise NotImplementedError
+
+    def build(self):
+        """
+        Constructs the full board
+        """
+        raise NotImplementedError
+
+
 class Tile:
     """
     Each tile on the board
