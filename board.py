71628288f9cc47ce9f68ae70fc8688c11d9bff2e
kerwei <kerwei84@gmail.com>
2020-07-04 16:29:37 +0800

    Move the Tile class into a separate module

--- board.py
+++ board.py
@@ -1,4 +1,5 @@
 import player
+import tile
 
 
 class Board:
@@ -9,7 +10,7 @@ class Board:
         # For now players are added based on list sequence. A method will be
         # added to determine the turn of each player later
         self.roster = {
-            i: player.Player() for i, name in lst_player
+            i: player.Player(token) for i, token in enumerate(lst_player)
         }
         self.schema = schema
 
@@ -24,72 +25,3 @@ class Board:
         Constructs the full board
         """
         raise NotImplementedError
-
-
-class Tile:
-    """
-    Each tile on the board
-    """
-    capacity = {
-        'house': 4,
-        'hotel': 1
-    }
-
-    def __init__(self, **kwargs):
-        self.name = kwargs['name']
-        self.color = kwargs['color']
-        self.cost = kwargs['cost']
-        self.schedule_fee = kwargs['schedule']
-
-        self.construct_count = {
-            'house': 0,
-            'hotel': 0
-        }
-        self._owner = None
-    
-    def liquidate(self):
-        """
-        Sell this tile. Returns the proceeds from the sale
-        """
-        self.owner = None
-        return self.cost['title']
-
-    def acquire(self, name: str):
-        """
-        Purchase this tile. Returns the cost of the title
-        """
-        self.owner = name
-        return self.cost['title']
-
-    def get_charges(self, n_tile: int):
-        """
-        Calculate the charges upon this visitor. n_tile is the number of same-
-        color tiles owned by the owner of this tile (to be supplied by the
-        Board class)
-        """
-        # Charges on the title
-        tile_fee = self.schedule_fee['title'] * n_tile
-
-        # Charges on the constructed properties
-        construct_fee = \
-            self.construct_count['house'] * self.schedule_fee['house'] \
-            + self.construct_count['hotel'] * self.schedule_fee['hotel']
-
-        return tile_fee + construct_fee
-
-    def add_construct(self, contype: str, qty: int=1):
-        """
-        Add house or hotel units to this tile. Returns the cost of construction
-        or 0 if the capacity is full
-        Rules:
-            1. House capacity: 4 Hotel capacity: 1
-            2. House capacity needs to be maxed first before a hotel can be
-               built
-        
-        """
-        if self.construct_count[contype] == capacity[contype]:
-            return 0
-
-        self.construct_count[contype] += 1
-
-        return self.cost[contype]
