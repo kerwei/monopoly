06e3891c7300c5a3ff0782fa87e76eaf5c3fb595
kerwei <kerwei84@gmail.com>
2020-08-16 00:17:22 +0800

    Add test case to check the integrity of the first token movements across the board

--- tests/test_board.py
+++ tests/test_board.py
@@ -114,3 +114,42 @@ class TestCreateBoard(unittest2.TestCase):
             self.new_board.player_location[first.token],
             6
         )
+
+    def testRaceOneLap(self):
+        """
+        Players race each other to make one full round across the board back
+        to the GO tile
+        """
+        self.new_board = board.Board(self.lst_token, schema=self.schema)
+
+        # Players take turns to move until one of them races past the GO tile
+        # for the first time
+        while set(self.new_board.player_nround.values()) == {1}:
+            active_player = self.new_board.player_roll.issue_next()
+            self.new_board.roll_till_move(active_player)
+
+        """
+        A minimum of 2 rounds will be required to race past the GO tile from
+        the starting spot. Therefore, trailing player locations will have a 
+        lower bound of 6 (rolling a 2 results in a bonus roll and thus, the 
+        lowest number of steps a player can make is 3). The location index of
+        the leader should not be greater than 39
+        """
+        # Check that the leader's location index is not greater than 39
+        leader = self.new_board.leader
+        self.assertLessEqual(
+            self.new_board.player_location[leader.token],
+            39
+        )
+
+        # Check that the last player in the race is not standing at location
+        # index of less than 6
+        last = self.new_board.last
+        self.assertGreaterEqual(
+            self.new_board.player_location[last.token],
+            6
+        )
+
+        # Check that the location index of all players fall between 0 - 39
+        self.assertTrue([
+            0 <= x <= 39 for x in self.new_board.player_location.keys()])
